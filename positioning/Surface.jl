c639c56e2d58aee92d91cab94154a0e6e763a708
Elias <elias.bjork@icloud.com>
2023-03-27 14:20:25 +0200

    Struct Surface created representing the physical properties of the RIS

---
+++ positioning/Surface.jl
@@ -0,0 +1,11 @@
+struct Surface
+    position::Vec2{Float64}
+    length::Float64
+    num_elements::Int64
+    element_width::Float64
+
+    function Surface(position::Vec2{Float64}, length::Float64, num_elements::Int64)
+        element_width = length / Float64(num_elements)
+        new(position, length, num_elements, element_width)
+    end
+end
