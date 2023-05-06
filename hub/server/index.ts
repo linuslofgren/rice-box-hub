fec0738827e9c57ea8964660245b2f640e0b41e3
Linus von Ekensteen Löfgren <pnl.lofgren@gmail.com>
2023-05-06 11:18:31 +0200

    Add redis client to server

--- hub/server/index.ts
+++ hub/server/index.ts
@@ -1,6 +1,12 @@
+import { connect } from "https://deno.land/x/redis@v0.29.3/mod.ts";
 import { hubServer } from "./server.ts";
 import { AppendingQueueController } from "./appendingQueueController.ts";
 import { Configuration } from "./types.ts";
+
+const redis = await connect({
+  hostname: "127.0.0.1",
+  port: 6379,
+});
 
 const { iterator, add } = AppendingQueueController<Configuration>();
 
@@ -8,6 +14,7 @@ const placementLoop = async () => {
   for await (const risConfiguration of iterator()) {
     // TODO: Send configuration to RIS via serial
     console.log("Configuration from JULIA", risConfiguration);
+    await redis.publish("ris_position", risConfiguration.join(","))
   }
 };
 
