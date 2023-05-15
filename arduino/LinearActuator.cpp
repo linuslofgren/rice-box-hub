2db8340623d9b9e339d5d0dc07f25a5dff1de6fa
Elias <elias.bjork@icloud.com>
2023-05-15 16:57:45 +0200

    arduino: add high level linear actuator interface

---
+++ arduino/LinearActuator.cpp
@@ -0,0 +1,34 @@
+#include "LinActStepper.cpp"
+
+class LinearActuator : private LinActStepper {
+    public:
+        float position;
+
+        LinearActuator(uint8_t stepPin, uint8_t dirPin, uint8_t stallPin) : LinActStepper(stepPin, dirPin, stallPin) {}
+
+        /*
+        Sets position in mm from front end. Positive direction is defined as "backwards" i.e. towards the motor.
+        Must be calibrated before use.
+        */
+        void setPosition(float newPosition) {
+            if (newPosition > position) {
+                setDirBackward();
+                step(newPosition-position);
+            } else {
+                setDirForward();
+                step(position-newPosition);
+            }
+            position = newPosition;
+        }
+
+        /*
+        NOT RECOMMENDED. Placeholder for actual calibration method incorporating stall detection.
+        This one just yeets the actuator into the wall and let's it stall until it's certainly at the end.
+        */
+        void calibratePositionCursed() {
+            setDirForward();
+            step(82.0);
+            position = 0.0;
+        }
+
+};
